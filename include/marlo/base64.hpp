4831d181348556f0bfd1d6f5dd5f99e807918d6c
bmarlo <54443351+bmarlo@users.noreply.github.com>
2023-01-08 12:05:09 -0300

    add overload

--- include/marlo/base64.hpp
+++ include/marlo/base64.hpp
@@ -17,17 +17,22 @@ std::string encode(std::string_view s);
 std::string encode(const std::uint8_t* data, std::size_t size);
 
 /**
+ * Encodes `s` into `dst`.
+ */
+void encode(std::string_view s, std::string& dst);
+
+/**
  * Encodes `data` into `dst`.
  */
 void encode(const std::uint8_t* data, std::size_t size, std::string& dst);
 
 /**
- * Decodes `s` into itself, returning whether it succeded.
+ * Decodes `s` into itself, returning whether it succeeded.
  */
 bool decode(std::string& s) noexcept;
 
 /**
- * Decodes `s` into `dst`, returning whether it succeded.
+ * Decodes `s` into `dst`, returning whether it succeeded.
  */
 bool decode(std::string_view s, std::uint8_t* dst) noexcept;
 
@@ -46,6 +51,11 @@ inline std::string encode(const std::uint8_t* data, std::size_t size)
     std::string result;
     encode(data, size, result);
     return result;
+}
+
+inline void encode(std::string_view s, std::string& dst)
+{
+    encode(reinterpret_cast<const std::uint8_t*>(s.data()), s.size(), dst);
 }
 
 }}
